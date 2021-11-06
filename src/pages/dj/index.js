b8aa8e704f0d27b82d9b9a57311acd2ccc6154d7
Prince Bazawule <prince.bazawule@gmail.com>
2021-11-06 15:47:52 -0400

    add conditional line-breaks

--- src/pages/dj/index.js
+++ src/pages/dj/index.js
@@ -50,8 +50,7 @@ export default function Dj({ data }) {
           </h1>
 
           <h2 className="mb-8 lg:mb-14 mt-4 xs:mt-2 text text-blueGray-900 dark:text-blueGray-100 tracking-tighter sm:tracking-tight font-black text-2xl leading-tight xs:text-3xl xs:leading-none sm:text-4xl sm:leading-none md:text-5xl md:leading-none lg:text-5xl lg:leading-none xl:text-5xl xl:leading-none 2xl:text-5xl 2xl:leading-none 3xl:text-6xl 3xl:leading-none 4xl:text-6xl 4xl:leading-none">
-            music provides the soundtrack to
-            <br /> everything i do
+            music provides the soundtrack to <span className='hidden sm:block'><br /></span> everything i do
           </h2>
 
           <p className="mb-8 text text-gray-800 dark:text-blueGray-100 tracking-tighter sm:tracking-tight font-normal text-xl leading-normal xs:text-2xl xs:leading-normal sm:text-2xl sm:leading-normal md:text-2xl md:leading-normal lg:text-2xl lg:leading-normal xl:text-2xl xl:leading-normal 2xl:text-3xl 2xl:leading-normal 3xl:text-3xl 3xl:leading-normal 4xl:text-3xl 4xl:leading-normal">
