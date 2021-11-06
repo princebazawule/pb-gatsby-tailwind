1fe3a1c702bae03150424f6aa88dd031c2a9298b
Prince Bazawule <prince.bazawule@gmail.com>
2021-11-06 15:04:22 -0400

    adhoc - test 1280

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,7 +21,7 @@ export default function Layout({ children }) {
     return (
         <>
         {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
-          <div className='selection:bg-yellow-100 selection:text-blueGray-800 dark:selection:bg-teal-200 dark:selection:text-blueGray-900 bg-teal-50 bg-opacity-80 dark:bg-coolGray-900 min-h-full flex flex-col justify-between m-0 p-0 opacity-0 transition-opacity duration-500' data-replace='{ "opacity-0" : "opacity-100" }'>
+          <div className='selection:bg-yellow-100 selection:text-blueGray-800 dark:selection:bg-teal-200 dark:selection:text-blueGray-900 bg-teal-50 xl:bg-red-400 bg-opacity-80 dark:bg-coolGray-900 min-h-full flex flex-col justify-between m-0 p-0 opacity-0 transition-opacity duration-500' data-replace='{ "opacity-0" : "opacity-100" }'>
               <Navbar />
               <div className="w-11/12 mx-auto flex-1 flex flex-col z-20">
                   { children }
