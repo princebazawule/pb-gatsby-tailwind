a695463f0897ee399e732db91b76833bf9766916
Prince Bazawule <prince.bazawule@gmail.com>
2021-11-09 17:03:13 -0400

    simplify offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -146,31 +146,32 @@ module.exports = {
     },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-netlify`,
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/blog/*`, `/contact/*`, `/designer/*`, `/developer/*`, `/dj/*`, `/post/*`, `/project/*`],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com/,
-            handler: 'cacheFirst',
-            options: {
-              cacheableResponse: {
-                statuses: [0, 200]
-              },
-              cacheName: 'google-fonts-webfonts',
-              expiration: {
-                maxAgeSeconds: 60 * 60,
-                maxEntries: 30
-              }
-            }
-          },
-        ],
-        workboxConfig: {
-          globPatterns: ['*.html', '**/src/images/*']
-       }
-      },
-    },
+    `gatsby-plugin-offline`,
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [`/blog/*`, `/contact/*`, `/designer/*`, `/developer/*`, `/dj/*`, `/post/*`, `/project/*`],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+    //         handler: 'cacheFirst',
+    //         options: {
+    //           cacheableResponse: {
+    //             statuses: [0, 200]
+    //           },
+    //           cacheName: 'google-fonts-webfonts',
+    //           expiration: {
+    //             maxAgeSeconds: 60 * 60,
+    //             maxEntries: 30
+    //           }
+    //         }
+    //       },
+    //     ],
+    //     workboxConfig: {
+    //       globPatterns: ['*.html', '**/src/images/*']
+    //    }
+    //   },
+    // },
     {
       resolve: 'gatsby-plugin-htaccess',
       options: {
